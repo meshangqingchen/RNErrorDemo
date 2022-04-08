b4280979076f400c3ff059bb93e936d34d03cb81
lichangcheng <lichangcheng@xiaomi.com>
2022-04-08 14:28:46 +0800

    、、

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+Sentry.init({
+    dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
+    integrations: [new BrowserTracing()],
+  
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
 AppRegistry.registerComponent(appName, () => App);
