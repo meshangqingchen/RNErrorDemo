d335b5cbcd33fa1b434731baffaa169c0fb0b987
lichangcheng <lichangcheng@xiaomi.com>
2022-10-26 16:39:56 +0800

    fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-// import App from './App';
+import App from './App';
 import AppTest from './AppTest'
 import {name as appName} from './app.json';
 
